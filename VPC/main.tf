b754168a1eb6ca57db189ff9cabd4ba7c0753fe1
MatveyGuralskiy <mathewguralskiy@gmail.com>
2024-03-25 20:07:11 +0200

    Add VPC Resource tf files with screens

---
+++ VPC/main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = var.Region
+  default_tags {
+    tags = {
+      Owner       = "Matvey Guralskiy"
+      Environment = var.Environment
+      From        = "Terraform"
+    }
+  }
+}
+
+resource "aws_vpc" "VPC" {
+  cidr_block = var.VPC_cidr
+  tags = {
+    Name = "My VPC - ${var.Environment}"
+  }
+}
+
+resource "aws_internet_gateway" "Gateway" {
+  vpc_id = aws_vpc.VPC.id
+  tags = {
+    Name = "My VPC - Internet Gateway"
+  }
+}
+
+resource "aws_subnet" "Public_Subnets" {
+  count                   = length(var.Public_Subnet_cidrs)
+  vpc_id                  = aws_vpc.VPC.id
+  cidr_block              = element(var.Public_Subnet_cidrs, count.index)
+  availability_zone       = data.aws_availability_zones.Availability.names[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "Public_Subnets" {
+  vpc_id = aws_vpc.VPC.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.Gateway.id
+  }
+  tags = {
+    Name = "Route Table - ${var.Environment}"
+  }
+}
+
+resource "aws_route_table_association" "Public_Routes" {
+  count          = length(aws_subnet.Public_Subnets[*].id)
+  route_table_id = aws_route_table.Public_Subnets.id
+  subnet_id      = element(aws_subnet.Public_Subnets[*].id, count.index)
+}
+
+data "aws_availability_zones" "Availability" {}
