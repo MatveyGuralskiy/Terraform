d9ae553be2509a637b4d1c240af603e7b299cbc9
MatveyGuralskiy <mathewguralskiy@gmail.com>
2024-03-27 23:03:15 +0200

    Add Terraform Cloud describe with screens

---
+++ Cloud/Github_Repository/variables.tf
@@ -0,0 +1,17 @@
+variable "Region" {
+  description = "Enter AWS Region you want to work with"
+  type        = string
+  default     = "eu-west-3"
+}
+
+variable "Instance_type" {
+  description = "AWS Instance type you want"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "Environment" {
+  description = "Environment type of Project"
+  type        = string
+  default     = "Production"
+}
